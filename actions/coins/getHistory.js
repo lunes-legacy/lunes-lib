b3597e6cac3e45983a25b4d3ab04ab7eccbccb6c
Luiz Fernando <luiz.silva@smarti9.com.br>
2018-01-23 19:04:59 -0200

    Resposta de gráfico formatada

---
+++ actions/coins/getHistory.js
@@ -0,0 +1,51 @@
+const axios = require('axios')
+const Promise = require('bluebird')
+
+const validator = require('../../services/validators/validator')
+const calendar = require('../../services/calendar')
+
+const endpoint = `${require('../../constants/Cryptocompare')}`
+
+module.exports = async (params) => {
+  const {fromDate, toDate, fromSymbol, toSymbol, exchange} = params
+
+  if (!fromDate || !toDate) {
+    throw new Error('The chart interval must be defined.')
+  }
+  const hourDiff = calendar.diff(toDate, fromDate, 'hours')
+  const historyType = hourDiff < 1 ? '/histominute?' : hourDiff >= 24 ? '/histoday?' : '/histohour?'
+  const query = [
+    'fsym=FROM_SYMBOL',
+    'tsym=TO_SYMBOL',
+    exchange && validator.isEmpty(exchange) ? `e=${exchange}` : ''
+  ]
+  const queryString = query
+    .reduce((q1, q2) => query.length > 0 ? q1 + '&' + q2 : q1)
+    .concat(`${calendar.getTimestamp(toDate)}`)
+    .replace('FROM_SYMBOL', fromSymbol || 'BTC')
+    .replace('TO_SYMBOL', toSymbol || 'USD')
+
+  const url = `${endpoint}${historyType}${queryString}`
+  try {
+    const res = await axios.get(url)
+    const mappedRes = await Promise.map(res.data.Data, d => {
+      delete d.close
+      delete d.open
+      delete d.volumefrom
+      delete d.volumeto
+      d.day = calendar.getCompleteDate(d.time)
+      delete d.time
+      return d
+    })
+    const data = await Promise.filter(mappedRes, r => !calendar.isBefore(r.day, fromDate))
+    return {
+      success: res.data.Response === 'Success',
+      status: res.status,
+      message: `Historical chart - ${fromSymbol || 'BTC'} to ${toSymbol || 'USD'}`,
+      period: `${fromDate} to ${toDate}`,
+      data
+    }
+  } catch (err) {
+    throw err.response ? err.response.data : new Error(err)
+  }
+}
--- actions/coins/index.js
+++ actions/coins/index.js
@@ -1,6 +1,8 @@
 const bitcoin = require('./bitcoin')
 const getPrice = require('./getPrice')
+const getHistory = require('./getHistory')
 module.exports = {
   bitcoin,
-  getPrice
+  getPrice,
+  getHistory
 }
