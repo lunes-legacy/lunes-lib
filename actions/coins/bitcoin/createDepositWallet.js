d66e2c7e0adf25dc236edd86f6b2535d703464c4
Daniel Laurindo <dslaurindo@gmail.com>
2018-02-22 00:43:08 -0300

    Improve depoist wallet service

--- actions/coins/bitcoin/createDepositWallet.js
+++ actions/coins/bitcoin/createDepositWallet.js
@@ -1,20 +1,21 @@
 const axios = require('axios')
 
-const createDepositWalletEndpoint = `${require('../../../constants/api')}/deposit/wallet/create`
+const createDepositWalletEndpointBTC = `${require('../../../constants/api')}/deposit/wallet/create/btc`
+const createDepositWalletEndpointETH = `${require('../../../constants/api')}/deposit/wallet/create/eth`
+const createDepositWalletEndpointLTC = `${require('../../../constants/api')}/deposit/wallet/create/ltc`
 
 /**
  *
  * @param {string} email - xxx@domain.com
  * @param {string} accessToken - hash token
- * @param {string} coin - btc, ltc, eth
  * @param {boolean} testnet -  true/false
  */
-module.exports = async (email, accessToken, coin, testnet) => {
+module.exports = async (email, accessToken, testnet) => {
 
   const headers = { Authorization: `Bearer ${accessToken}` }
   try {
     // 1. create transaction
-    const depositResponse = await create(headers, email, coin, testnet)
+    const depositResponse = await create(headers, email, testnet)
 
     return depositResponse
   } catch (err) {
@@ -25,14 +26,18 @@ module.exports = async (email, accessToken, coin, testnet) => {
 /**
  * @param {object} headers - { Authorization: 'Bearer jkhjhkhkhkjn' }
  * @param {string} email - xxx@domain.com
- * @param {string} coin - btc, ltc, eth
  * @param {boolean} testnet -  true/false
 */
-const create = async (headers, email, coin, testnet) => {
+const create = async (headers, email, testnet) => {
   try {
-    const url = `${createDepositWalletEndpoint}/${coin}`
-    const res = await axios.post(createDepositWalletEndpoint, {email, testnet}, { headers })
-    return res.data
+    const resBTC = await axios.post(createDepositWalletEndpointBTC, {email, testnet}, { headers })
+    //const resETH = await axios.post(createDepositWalletEndpointETH, {email, testnet}, { headers })
+    //const resLTC = await axios.post(createDepositWalletEndpointLTC, {email, testnet}, { headers })
+    return {
+      BTC: resBTC.data,
+      ETH: null,
+      LTC: null
+    };
   } catch (err) {
     throw err.response ? err.response.data : new Error(err)
   }
