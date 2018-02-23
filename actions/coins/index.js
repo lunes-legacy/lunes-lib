9308178723ad99d132cf60bb09ae5ea30b235b34
Felipe Matheus Rosa <felipe.mr89@gmail.com>
2018-02-23 09:56:55 -0300

    add to index

--- actions/coins/index.js
+++ actions/coins/index.js
@@ -3,10 +3,13 @@ const getPrice = require('./getPrice')
 const getHistory = require('./getHistory')
 const obtainWallet = require('./obtainWallet')
 const getFees = require('./getFees')
+const createDepositWallet = require('./createDepositWallet')
+
 module.exports = {
   bitcoin,
   obtainWallet,
   getPrice,
   getHistory,
-  getFees
+  getFees,
+  createDepositWallet
 }
---
+++ actions/ico/index.js
@@ -0,0 +1,12 @@
+const buy = require('./buyCoin')
+const buyConversion = require('./buyConversion')
+const buyHistory = require('./buyHistory')
+const confirmTerm = require('./confirmTerm')
+const obtainPhase = require('./obtainPhase')
+module.exports = {
+  buy,
+  buyConversion,
+  buyHistory,
+  confirmTerm,
+  obtainPhase
+}
