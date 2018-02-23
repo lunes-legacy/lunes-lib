09f4256c4962062e0024faf716fb46ed88b4bcbb
Wandyer Silva <wandyer.s@gmail.com>
2018-02-23 01:20:10 -0300

    Updating convertion

--- actions/ico/buyConversion.js
+++ actions/ico/buyConversion.js
@@ -45,6 +45,11 @@ const fromLNS = (bonusRate, inputAmount, exchangeRate, unitPrice, coupon) => {
     bonusRate += 0.01
   }
 
+  bonusRate = Number(bonusRate)
+  inputAmount = Number(inputAmount)
+  exchangeRate = Number(exchangeRate)
+  unitPrice = Number(unitPrice)
+
   let exchangeUnitPrice = unitPrice / exchangeRate
   let buyAmount = inputAmount * exchangeUnitPrice
   let bonusAmount = buyAmount * bonusRate
