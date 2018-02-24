3b837c447396846b21702c06d4073d4e9c9a8b16
Felipe Matheus Rosa <felipe.mr89@gmail.com>
2018-02-24 20:55:16 -0300

    commit index

--- actions/coins/index.js
+++ actions/coins/index.js
@@ -4,11 +4,14 @@ const getHistory = require('./getHistory')
 const obtainWallet = require('./obtainWallet')
 const getFees = require('./getFees')
 const createDepositWallet = require('./createDepositWallet')
+const getBalance = require('./getBalance')
+
 module.exports = {
   bitcoin,
   obtainWallet,
   getPrice,
   getHistory,
   getFees,
-  createDepositWallet
+  createDepositWallet,
+  getBalance
 }
