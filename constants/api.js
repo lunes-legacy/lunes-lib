fd2fa0a064a2698c6f32c5e14a56cfdc5987eb8e
Daniel Laurindo <dslaurindo@gmail.com>
2018-04-15 15:16:19 -0300

    Change API url

--- constants/api.js
+++ constants/api.js
@@ -2,4 +2,4 @@ const URL_WALLET = 'https://api.lunes.io/api'
 const URL_ICO_PURCHASE = 'https://apiw.lunes.io/api'
 const URL_LOCAL_TEST = 'http://localhost:6001/api'
 
-module.exports = URL_LOCAL_TEST
+module.exports = URL_WALLET
