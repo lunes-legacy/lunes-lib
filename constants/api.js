6fdb5c5d5d0495e1fc37326c3c2ed427848fa106
Daniel Laurindo <dslaurindo@gmail.com>
2018-04-16 22:21:09 -0300

    Add URL

--- constants/api.js
+++ constants/api.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-module.exports = 'https://api.lunes.io/api'
-=======
 const URL_WALLET = 'https://api.lunes.io/api'
 const URL_ICO_PURCHASE = 'https://apiw.lunes.io/api'
 const URL_LOCAL_TEST = 'http://localhost:6001/api'
 
 module.exports = URL_WALLET
->>>>>>> 43acb1f3d9a94fac984a346a977928143fec7efc
