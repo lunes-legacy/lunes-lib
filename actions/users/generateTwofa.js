add8c31b27d0c112030f0fed2a218c773c0f0f3e
Daniel Laurindo <dslaurindo@gmail.com>
2018-04-16 22:14:04 -0300

    Finished twofa

--- actions/users/generateTwofa.js
+++ actions/users/generateTwofa.js
@@ -4,10 +4,13 @@ const BASE_URL = require('../../constants/api')
 
 const endpoint = `${BASE_URL}/auth2step/generate`
 
-module.exports = async (userData) => {
-  const { email, twofa } = userData
+module.exports = async (email, generateNew) => {
+  let url = `${endpoint}?email=${email}`;
+  if (generateNew) {
+    url = `${url}&new=true`
+  }
   try {
-    const res = await axios.get(`${endpoint}?email=${email}`)
+    const res = await axios.get(url)
     return res.data
   } catch (err) {
     throw err.response ? err.response.data : new Error(err)
