6b9c3c9fa5967c387b54c8c13f9d47fab381875f
Wandyer Silva <wandyer.s@gmail.com>
2018-06-04 19:54:07 -0300

    Fixing segwit address creation

--- services/wallet/btc/wallet.js
+++ services/wallet/btc/wallet.js
@@ -8,9 +8,18 @@ const MnemonicService = require('../mnemonic')
  * @param {*} coin - coin
  * TODO: segwit or not parameter
  */
-const newAddress = (mnemonic, coin) => {
-  const keyPair = mnemonicToKeyPair(mnemonic, coin)
-  const address = keyPair.getAddress()
+const newAddress = (mnemonic, network) => {
+  const keyPair = mnemonicToKeyPair(mnemonic, network)
+  let redeemScript = bitcoinjs.script.witnessPubKeyHash.output.encode(
+    bitcoinjs.crypto.hash160(keyPair.getPublicKeyBuffer())
+  )
+  let scriptPubKey = bitcoinjs.script.scriptHash.output.encode(
+    bitcoinjs.crypto.hash160(redeemScript)
+  )
+  let address = bitcoinjs.address.fromOutputScript(
+    scriptPubKey,
+    network.bitcoinjsNetwork
+  )
   return address
 }
 
@@ -22,22 +31,9 @@ const newAddress = (mnemonic, coin) => {
  */
 const mnemonicToKeyPair = (mnemonic, coin) => {
   const network = coin.bitcoinjsNetwork
-  // const seedHex = MnemonicService.mnemonicToSeedHex(mnemonic)
-  // const hdNode = bitcoin.HDNode.fromSeedHex(seedHex, network)
-  // const keyPair = hdNode.derivePath(coin.derivePath + '/0')
-
   var seed = MnemonicService.mnemonicToSeed(mnemonic)
   let hdNode = bitcoinjs.HDNode.fromSeedBuffer(seed, network)
   let keyPair = hdNode.derivePath(coin.derivePath + '/0')
-
-  let redeemScript = bitcoinjs.script.witnessPubKeyHash.output.encode(
-    bitcoinjs.crypto.hash160(keyPair.getPublicKeyBuffer())
-  )
-  let scriptPubKey = bitcoinjs.script.scriptHash.output.encode(
-    bitcoinjs.crypto.hash160(redeemScript)
-  )
-  let segwitAddress = bitcoinjs.address.fromOutputScript(scriptPubKey, network)
-
   return keyPair
 }
 
