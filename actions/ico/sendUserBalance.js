489fe3894388b7417d67d54706ef3308075bfad7
Wandyer Silva <wandyer.s@gmail.com>
2018-06-06 21:25:33 -0300

    Fixing header

--- actions/ico/sendUserBalance.js
+++ actions/ico/sendUserBalance.js
@@ -8,7 +8,7 @@ const endpoint = `${require('../../constants/api')}/ico/send-user-balance`
 module.exports = async accessToken => {
   const headers = { Authorization: `Bearer ${accessToken}` }
   try {
-    const res = await axios.post(endpoint, { headers })
+    const res = await axios.post(endpoint, '', { headers })
     return res.data
   } catch (err) {
     throw err.response ? err.response.data : new Error(err)
