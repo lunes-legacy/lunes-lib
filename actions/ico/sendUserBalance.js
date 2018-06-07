854b72f1a6f99be84a129b4d4ad3e7adb6f5dff5
Wandyer Silva <wandyer.s@gmail.com>
2018-06-06 21:16:30 -0300

    Removing unused object

--- actions/ico/sendUserBalance.js
+++ actions/ico/sendUserBalance.js
@@ -5,10 +5,10 @@ const endpoint = `${require('../../constants/api')}/ico/send-user-balance`
 /**
  *TODO:
  */
-module.exports = async (data, accessToken) => {
+module.exports = async accessToken => {
   const headers = { Authorization: `Bearer ${accessToken}` }
   try {
-    const res = await axios.post(endpoint, data, { headers })
+    const res = await axios.post(endpoint, { headers })
     return res.data
   } catch (err) {
     throw err.response ? err.response.data : new Error(err)
