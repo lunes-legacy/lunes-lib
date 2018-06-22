bde0e6e808b53b5224638971952c8d36c3a502a5
Rodrigo Mingattos <rodrigo_mingattos@hotmail.com>
2018-06-21 22:40:34 -0300

    Remove try/catch desnecessario

--- services/wallet/eth/balance.js
+++ services/wallet/eth/balance.js
@@ -2,18 +2,18 @@ const Web3 = require('web3')
 const errorPattern = require('../../errorPattern')
 
 module.exports = async (address, network) => {
-  try {
-    return (new Web3(
-      new Web3.providers.HttpProvider(
-        network.apiUrl
-      )
-    )).eth.getBalance(address);
-  } catch (error) {
-    throw errorPattern(
-      error.message || 'Error retrieving balances',
-      error.status || 500,
-      error.messageKey || 'BALANCE_ERROR',
-      error.logMessage || error.stack || ''
+  let balance = await (new Web3(
+    new Web3.providers.HttpProvider(
+      network.apiUrl
     )
-  }
+  )).eth.getBalance(address);
+
+  return {
+    network: network.coinSymbol,
+    data: {
+      address: address,
+      confirmed: balance.toString(),
+      unconfirmed: null
+    }
+  };
 }
--- services/wallet/lns/balance.js
+++ services/wallet/lns/balance.js
@@ -17,39 +17,30 @@ const errorPattern = require('../../errorPattern')
       }
  */
 module.exports = async (address, network) => {
-  try {
-    const validate = await validateAddress(address, network)
-    if (!validate) {
-      throw errorPattern(
-        'Invalid ' + network.coinName + ' Address',
-        406,
-        'ADDRESS_INVALID',
-        'The address ' +
-          address +
-          ' is not a valid ' +
-          network.coinName +
-          ' address.'
-      )
-    }
-
-    let res = await axios.get(
-      network.apiUrl + '/addresses/balance/details/' + address
-    )
-
-    return {
-      network: network.coinSymbol,
-      data: {
-        address: address,
-        confirmed: res.data.available,
-        unconfirmed: null
-      }
-    }
-  } catch (error) {
+  const validate = await validateAddress(address, network)
+  if (!validate) {
     throw errorPattern(
-      error.message || 'Error retrieving balances',
-      error.status || 500,
-      error.messageKey || 'BALANCE_ERROR',
-      error.logMessage || error.stack || ''
+      'Invalid ' + network.coinName + ' Address',
+      406,
+      'ADDRESS_INVALID',
+      'The address ' +
+        address +
+        ' is not a valid ' +
+        network.coinName +
+        ' address.'
     )
+  }
+
+  let res = await axios.get(
+    network.apiUrl + '/addresses/balance/details/' + address
+  )
+
+  return {
+    network: network.coinSymbol,
+    data: {
+      address: address,
+      confirmed: res.data.available,
+      unconfirmed: null
+    }
   }
 }
