d679492493014a64b3d4bd651cdccd43bb430258
Rodrigo Mingattos <rodrigo.cesario@lunes.io>
2018-06-22 16:28:57 -0300

    Altera o retorno do balance BTC para Satoshi

--- services/wallet/btc/balance.js
+++ services/wallet/btc/balance.js
@@ -6,14 +6,14 @@ module.exports = async (address, network) => {
   const electrum = await ElectrumClient(network)
   const balance = await electrum.blockchainAddress_getBalance(address)
   electrum.close();
-  
+
   return {
     status: 'success',
     data: {
       network: network.coinSymbol,
       address: address,
-      confirmed_balance: sb.toBitcoin(balance.confirmed).toFixed(8),
-      unconfirmed_balance: sb.toBitcoin(balance.unconfirmed).toFixed(8)
+      confirmed_balance: balance.confirmed.toString(),
+      unconfirmed_balance: balance.unconfirmed.toString()
     }
   };
 }
