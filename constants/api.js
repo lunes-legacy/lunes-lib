13dfb595dbb3fec937952cbce7be41b37913a0b0
Wandyer Silva <wandyer.s@gmail.com>
2018-07-09 11:23:20 -0300

    Updating to production API

--- constants/api.js
+++ constants/api.js
@@ -1,6 +1,6 @@
 const URL_WALLET = 'https://api.lunes.io/api'
-const URL_ICO_PURCHASE = 'https://apiw.lunes.io/api'
+const URL_PRODUCTION = 'https://apiw.lunes.io/api'
 const URL_LOCAL_TEST = 'http://localhost:6001/api'
 const URL_STAGING = 'https://api-stage.lunes.io/api'
 
-module.exports = URL_STAGING
+module.exports = URL_PRODUCTION
