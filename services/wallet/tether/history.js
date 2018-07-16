d63c30c3bff2336d85780573ae31dfc57b069a9a
Marcelo <marcelorafaeldebrito@gmail.com>
2018-07-16 16:31:22 -0300

    Added some ctrl of errors in the balance, remove try/catch from address transactions history

--- services/wallet/tether/history.js
+++ services/wallet/tether/history.js
@@ -39,8 +39,8 @@ const arrangeTransactionsToReturn = (transactions) => {
       blockHeight:  undefined,
       txid:         tx.txid,
       date:         tx.blocktime,
-      nativeAmount: unitConverter.toSatoshi(tx.amount),
-      networkFee:   unitConverter.toSatoshi(tx.fee),
+      nativeAmount: unitConverter.toSatoshi(tx.amount).toString(),
+      networkFee:   unitConverter.toSatoshi(tx.fee).toString(),
       otherParams: {
         propertyid:       tx.propertyid,
         propertyname:     tx.propertyname,
@@ -51,46 +51,39 @@ const arrangeTransactionsToReturn = (transactions) => {
   });
 }
 module.exports = async (params) => {
-  try {
-    if (!params.address)
-      throw errorPattern(`Invalid address, got '${params.address}'`);
-    if (!params.testnet)
-      throw errorPattern(`Invalid testnet parameter, got '${params.testnet}'`);
+  if (!params.address)
+    throw errorPattern(`Invalid address, got '${params.address}'`);
+  if (!params.testnet)
+    throw errorPattern(`Invalid testnet parameter, got '${params.testnet}'`);
 
-    let requestParams = new URLSearchParams();
-    requestParams.append('addr', params.address);
-    requestParams.append('page', 0);
+  let requestParams = new URLSearchParams();
+  requestParams.append('addr', params.address);
+  requestParams.append('page', 0);
 
-    let result = await Axios.post('/v1/transaction/address', requestParams)
-      .catch((e) => {
-        let { status, statusText, headers } = e.response;
-        if (status !== 200)
-          throw errorPattern('Request error',status,'TXHISTORY_ERROR', {statusText, headers});
-        throw errorPattern(statusText, status, 'TXHISTORY_ERROR', headers);
-      });
+  let result = await Axios.post('/v1/transaction/address', requestParams)
+    .catch((e) => {
+      let { status, statusText, headers } = e.response;
+      if (status !== 200)
+        throw errorPattern('Request error',status,'TXHISTORY_ERROR', {statusText, headers});
+      throw errorPattern(statusText, status, 'TXHISTORY_ERROR', headers);
+    });
 
-    let transactions = result.data.transactions;
-    if (transactions instanceof String || typeof transactions === 'string')
-      transactions = JSON.parse(transactions);
+  let transactions = result.data.transactions;
+  if (transactions instanceof String || typeof transactions === 'string')
+    transactions = JSON.parse(transactions);
 
-    transactions = onlyTetherUSTransactions(transactions);
-    transactions = identifyTransactionType(transactions, params.address);
-    transactions = arrangeTransactionsToReturn(transactions);
+  transactions = onlyTetherUSTransactions(transactions);
+  transactions = identifyTransactionType(transactions, params.address);
+  transactions = arrangeTransactionsToReturn(transactions);
 
-    let network = params.network;
-    let data    = {
-      address: params.address,
-      history: transactions
-    }
+  let network = params.network;
+  let data    = {
+    address: params.address,
+    history: transactions
+  }
 
-    return {
-      network,
-      data
-    }
-  } catch(e) {
-    //if this error is already an errorPattern's object, so we return it
-    if (e.message && e.status && e.messageKey)
-      return e;
-    return errorPattern('Error on trying to get transaction history',500,'TXHISTORY_ERROR',e);
+  return {
+    network,
+    data
   }
 }
