911cfd0531293cfe43b7d989a34fb54655c74689
Marcelo <marcelorafaeldebrito@gmail.com>
2018-07-16 16:31:22 -0300

    Added some ctrl of errors in the balance, remove try/catch from address transactions history

--- services/wallet/tether/balance.js
+++ services/wallet/tether/balance.js
@@ -1,15 +1,34 @@
+require('module-alias/register');
+const errorPattern = require('@Services/errorPattern.js');
 const Axios = require('./axios');
 
+const onlyTetherBalance = (balance) => {
+  return balance.filter((obj) => {
+    return obj.symbol.search(/(SP31)/i) !== -1
+  });
+}
+
 module.exports = async (address, network) => {
+  if (!address)
+    throw errorPattern(`Got '${address}' from address variable`,500,'BALANCE_ERROR');
+  if (!network)
+    throw errorPattern(`Got '${network}' from network variable`,500,'BALANCE_ERROR');
+
   let params = new URLSearchParams();
   params.append('addr', address);
-  let result = await Axios.post('/v1/address/addr/', params);
-  let tether = result.data.balance.filter((obj) => {
-    return obj.symbol === 'SP31';
-  });
+
+  let result = await Axios.post('/v1/address/addr/', params)
+    .catch((e) => {
+      let { status, message, messageKey, headers } = e.response;
+      if (status !== 200)
+        throw errorPattern('Request error', status, 'BALANCE_ERROR', { message, messageKey, headers });
+
+      throw errorPattern(message, status, 'BALANCE_ERROR', {messageKey, headers});
+    });
+
+  let tether = onlyTetherBalance(result.data.balance);
   tether = tether[0];
-  // tether.pendingpos = '10000000';
-  // tether.pendingneg = '100000000';
+
   let balance = {
     network: network.coinSymbol.toUpperCase(),
     data: {
