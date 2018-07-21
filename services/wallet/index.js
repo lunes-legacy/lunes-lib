d379d5b6b96e62f7655152c0239cae72914b5932
Felipe Matheus Rosa <felipe@lunes.io>
2018-07-21 20:08:20 -0300

    add bch export

--- services/wallet/index.js
+++ services/wallet/index.js
@@ -1,10 +1,12 @@
 const btc = require('./btc')
+const bch = require('./bch')
 const eth = require('./eth')
 const lns = require('./lns')
 const mnemonic = require('./mnemonic')
 
 module.exports = {
   btc,
+  bch,
   eth,
   lns,
   mnemonic
