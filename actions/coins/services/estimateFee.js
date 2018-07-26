0048701d9d3f2b7a7dad5703b4daa1ce6447854b
Marcelo <marcelorafaeldebrito@gmail.com>
2018-07-25 21:57:53 -0300

    Changed throw by return inside estimateFee

--- actions/coins/services/estimateFee.js
+++ actions/coins/services/estimateFee.js
@@ -60,6 +60,7 @@ module.exports = async (transactionData, accessToken) => {
     const res = await axios.post(endpoint, transactionData, { headers })
     return res.data
   } catch (err) {
-    throw err.response ? err.response.data : new Error(err)
+    // throw err.response ? err.response.data : new Error(err)
+    return err.response ? err.response.data : err
   }
 }
