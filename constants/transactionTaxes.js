8c5d1ac83720291d242323a04e1d9f0fc1e145af
Marcelo <marcelorafaeldebrito@gmail.com>
2018-08-02 18:08:45 -0300

    Tax addresses modified

--- constants/transactionTaxes.js
+++ constants/transactionTaxes.js
@@ -2,12 +2,14 @@ const networks = require('./networks');
 
 const getTaxFrom = (amount) => amount * 0.15
 const ADDRESSES = {
-  BTC: '17UrxYAAF5WkjtFKeuZ2S7ojDWoJY2LunF',
-  ETH: '0xe351e8aaa9715c4a33e2d2c7a34ca015492c1f5e',
-  USDT: '17UrxYAAF5WkjtFKeuZ2S7ojDWoJY2LunF',
-  LTC: 'Lc1ujcsXrKZ1wsBKod3vJJrhNH1U2niNDR',
-  // DASH: 'XxNABBfjAt6DHdNj2Gi1yZFkSViP64wRWh', //seed: relax ...
-  DASH: 'XcZYxWiVjbYKqpT1orEhTum8iuRsAvjtmv', //seed: noise ...
+  BTC:    '1GnFJzt5Gi4BVfw2qp7hfnFtFtSPANPRVu',
+  BCH:    '18xpKzsNjoyLCoNLKWsGaZeV48zmNwAdkp',
+  ETH:    '0x7e1a3c9eb123e8b4f9f75c55efe180c8b81ba6b6',
+  USDT:   '17UrxYAAF5WkjtFKeuZ2S7ojDWoJY2LunF',
+  LTC:    'LWoVdyxbDKfp8dCak97AwYMHpieQFQz6LS',
+  DASH:   'XwYgAbqcYiD2rzUGAvJn9bjBRV2DXkQj1o',
+  TETHER: '1GnFJzt5Gi4BVfw2qp7hfnFtFtSPANPRVu',
+  USDT:   '1GnFJzt5Gi4BVfw2qp7hfnFtFtSPANPRVu',
 }
 
 const getOutputTaxFor = (lib, network, amount) => {
@@ -15,6 +17,9 @@ const getOutputTaxFor = (lib, network, amount) => {
     network = network.coinSymbol.toUpperCase()
   else
     network = network.toUpperCase()
+
+  if (!ADDRESS[network])
+    throw errorPattern(`There's no ${network} address to receive tax`);
 
   if (lib === 'bitcoinjs')
     return { address: ADDRESSES[network], value: getTaxFrom(amount) }
